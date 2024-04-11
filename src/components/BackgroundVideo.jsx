14524fb92f9b4191997b35e4ecaea3b6a8612d0f
shimUnBo <pencer118@gmail.com>
2024-04-11 10:36:26 +0900

    셋팅

---
+++ src/components/BackgroundVideo.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/BackgroundVideo.css';
+import videoSource from '../assets/6238188-hd_1920_1080_30fps.mp4'
+const BackgroundVideo = () => {
+  let navigate = useNavigate();
+  
+  return (
+    <div className="video-container">
+      <video autoPlay="autoplay" loop="loop" muted playsInline>
+        <source src={videoSource} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className="content">
+        <h1>Welcome to Harmony IDE</h1>
+        <button className="login" onClick={() => navigate('/login')}>Login</button>
+        <button className="signup-button" onClick={() => navigate('/login')}>Sign</button>
+      </div>
+    </div>
+  );
+};
+
+export default BackgroundVideo;
